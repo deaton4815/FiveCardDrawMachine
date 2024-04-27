c44b786426242c78c4436530fcb5441cb1eee86c
Philip Deaton <philipdeaton826@gmail.com>
2024-04-27 18:05:15 -0400

    Basic dealer function for dealing new hand

--- Dealer.cpp
+++ Dealer.cpp
@@ -1,1 +1,8 @@
 #include "Dealer.h"
+
+using namespace std;
+
+void Dealer::dealHand() {
+	vector<Card> dealt = m_deck.getCards(m_hand.getHandSize());
+	m_hand.newHand(dealt);
+}
--- Dealer.h
+++ Dealer.h
@@ -1,5 +1,19 @@
 #pragma once
+
+#include <vector>
+#include "CardDeck.h"
+#include "Card.h"
+#include "Hand.h"
+
 class Dealer
 {
+public:
+
+	void dealHand();
+
+private:
+
+	CardDeck m_deck;
+	Hand m_hand;
 };
 
