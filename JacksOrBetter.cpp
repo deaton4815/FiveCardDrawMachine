64bf0d0a93e2bb9a449b62876bee6bb64e097340
Philip Deaton <philipdeaton826@gmail.com>
2024-04-28 18:55:17 -0400

    Jacks or better class

--- JacksOrBetter.cpp
+++ JacksOrBetter.cpp
@@ -1,1 +1,21 @@
 #include "JacksOrBetter.h"
+
+using namespace std;
+
+bool JacksOrBetter::isMatch(vector<Card> hand) const {
+	bool jacksOrBetter{ false };
+
+	vector<unsigned> ranks{ getSortedRanks(hand) };
+	vector<unsigned> highRanks;
+	highRanks.reserve(ranks.size());
+	for (auto rank{ cbegin(ranks) }; rank != cend(ranks); ++rank) {
+		if ((1 == (*rank)) || (*rank > 10)) {
+			highRanks.push_back(*rank);
+		}
+	}
+
+	if (highRanks.size() > 1) {
+		jacksOrBetter = hasNMatches(highRanks, 2);
+	}
+	return jacksOrBetter;
+}
--- JacksOrBetter.h
+++ JacksOrBetter.h
@@ -1,5 +1,13 @@
 #pragma once
-class JacksOrBetter
+
+#include "PokerHand.h"
+
+class JacksOrBetter : public PokerHand
 {
+public:
+	bool isMatch(std::vector<Card>) const;
+private:
+	unsigned m_multiplier{ 1 };
+	std::string m_name{ "Jacks or Better" };
 };
 
